8e1db585ed45307bfa032543423a957990c60763
lester1508 <140740945+lester1508@users.noreply.github.com>
2024-08-16 23:50:58 -0600

    Update and rename repositorioTipoPlatillo.java to TipoPlatilloRepositorio.java

--- restaurante/src/main/java/hn/restaurante/restaurante/repositorios/TipoPlatilloRepositorio.java
+++ restaurante/src/main/java/hn/restaurante/restaurante/repositorios/TipoPlatilloRepositorio.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import hn.restaurante.restaurante.modelos.TipoPlatillo;
 
-public interface repositorioTipoPlatillo extends JpaRepository<TipoPlatillo, Integer> {
+public interface TipoPlatilloRepositorio extends JpaRepository<TipoPlatillo, Integer> {
 }
