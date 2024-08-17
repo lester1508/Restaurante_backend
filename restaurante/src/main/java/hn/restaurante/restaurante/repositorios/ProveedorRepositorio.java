f7aafe977cdd3537e9c4b77008580752bedbb23d
lester1508 <140740945+lester1508@users.noreply.github.com>
2024-08-16 23:49:42 -0600

    Update and rename repositorioProveedor.java to ProveedorRepositorio.java

--- restaurante/src/main/java/hn/restaurante/restaurante/repositorios/ProveedorRepositorio.java
+++ restaurante/src/main/java/hn/restaurante/restaurante/repositorios/ProveedorRepositorio.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import hn.restaurante.restaurante.modelos.Proveedor;
 
-public interface repositorioProveedor extends JpaRepository<Proveedor, Integer> {
+public interface ProveedorRepositorio extends JpaRepository<Proveedor, Integer> {
 }
 
