8a9d987c2615dca719949579f911b56ea6cb0ab3
lester1508 <140740945+lester1508@users.noreply.github.com>
2024-08-16 23:44:23 -0600

    Update and rename repositorioIngrediente.java to IngredienteRepositorio.java

--- restaurante/src/main/java/hn/restaurante/restaurante/repositorios/IngredienteRepositorio.java
+++ restaurante/src/main/java/hn/restaurante/restaurante/repositorios/IngredienteRepositorio.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import hn.restaurante.restaurante.modelos.Ingrediente;
 
-public interface repositorioIngrediente extends JpaRepository<Ingrediente, Integer> {
+public interface IngredienteRepositorio extends JpaRepository<Ingrediente, Integer> {
 }
 
