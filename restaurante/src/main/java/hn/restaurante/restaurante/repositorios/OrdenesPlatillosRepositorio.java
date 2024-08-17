878a167b0bac158d7e7b51b00515b5665ffae787
lester1508 <140740945+lester1508@users.noreply.github.com>
2024-08-16 23:46:00 -0600

    Update and rename repositorioOrdenesPlatillos.java to OrdenesPlatillosRepositorio.java

--- restaurante/src/main/java/hn/restaurante/restaurante/repositorios/OrdenesPlatillosRepositorio.java
+++ restaurante/src/main/java/hn/restaurante/restaurante/repositorios/OrdenesPlatillosRepositorio.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import hn.restaurante.restaurante.modelos.OrdenesPlatillos;
 import hn.restaurante.restaurante.modelos.OrdenesPlatillosId;
 
-public interface repositorioOrdenesPlatillos extends JpaRepository<OrdenesPlatillos, OrdenesPlatillosId> {
+public interface OrdenesPlatillosRepositorio extends JpaRepository<OrdenesPlatillos, OrdenesPlatillosId> {
 }
 
