24015b92a62ee0161353b47b64df578d40819dfa
marianosorio <149555450+marianosorio@users.noreply.github.com>
2024-08-17 16:55:51 -0600

    Add files via upload

---
+++ restaurante/src/main/java/hn/restaurante/restaurante/controladores/MesaControlador.java
@@ -0,0 +1,32 @@
+package hn.restaurante.restaurante.restaurante.controladores;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hn.restaurante.restaurante.restaurante.modelos.Mesa;
+import hn.restaurante.restaurante.restaurante.modelos.Platillo;
+import hn.restaurante.restaurante.restaurante.servicios.MesaServicio;
+
+@RestController
+@RequestMapping("/api/mesas")
+public class MesaControlador {
+
+    @Autowired
+    private MesaServicio mesaServicio;
+
+     @GetMapping("/obtener/todos")
+    public List <Mesa> obtenerTodos(){
+        return this.mesaServicio.obtenerTodos();
+    }
+
+     @PostMapping("/crear/mesa")
+        public Mesa crearMesa(@RequestBody Mesa nuevaMesa){
+            return this.mesaServicio.crearMesa(nuevaMesa);
+        }
+    
+}
