1b234ce6ae6d695367e5ea744b9df248c04b35d9
lester1508 <140740945+lester1508@users.noreply.github.com>
2024-08-16 23:58:10 -0600

    Update TipoPlatilloServicio.java

--- restaurante/src/main/java/hn/restaurante/restaurante/servicios/TipoPlatilloServicio.java
+++ restaurante/src/main/java/hn/restaurante/restaurante/servicios/TipoPlatilloServicio.java
@@ -7,24 +7,24 @@ import org.springframework.stereotype.Service;
 
 import hn.restaurante.restaurante.modelos.TipoPlatillo;
 import hn.restaurante.restaurante.modelos.TipoUsuario;
-import hn.restaurante.restaurante.repositorios.repositorioTipoPlatillo;
-import hn.restaurante.restaurante.repositorios.repositorioTipoUsuario;
+import hn.restaurante.restaurante.repositorios.TipoPlatilloRepositorio;
+import hn.restaurante.restaurante.repositorios.TipoUsuarioRepositorio;
 
 @Service
 public class TipoPlatilloServicio {
 
     @Autowired
-    private repositorioTipoPlatillo TipoPlatilloRepositorio;
+    private TipoUsuarioRepositorio tipoPlatilloRepositorio;
 
      public List <TipoPlatillo> obtenerTodos(){
-        return this.TipoPlatilloRepositorio.findAll();
+        return this.tipoPlatilloRepositorio.findAll();
     }
 
     public TipoPlatillo crearTipoPlatillo(TipoPlatillo NuevoTipoPlatillo){
-        if(this.TipoPlatilloRepositorio.existsById(NuevoTipoPlatillo.getIdTipoPlatillo())){
+        if(this.tipoPlatilloRepositorio.existsById(NuevoTipoPlatillo.getIdTipoPlatillo())){
             return null;
         }
-        return this.TipoPlatilloRepositorio.save(NuevoTipoPlatillo);
+        return this.tipoPlatilloRepositorio.save(NuevoTipoPlatillo);
     }
     
 }
