debe10df597769a3f292c958088331c61e183cce
marianosorio <149555450+marianosorio@users.noreply.github.com>
2024-08-17 17:50:02 -0600

    Add files via upload

---
+++ restaurante/src/main/java/hn/restaurante/restaurante/controladores/ReservacionControlador.java
@@ -0,0 +1,33 @@
+package hn.restaurante.restaurante.restaurante.controladores;
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hn.restaurante.restaurante.restaurante.modelos.Reservacion;
+import hn.restaurante.restaurante.restaurante.modelos.TipoPlatillo;
+import hn.restaurante.restaurante.restaurante.servicios.ReservacionServicio;
+
+@RestController
+@RequestMapping("/api/reservaciones")
+public class ReservacionControlador {
+    
+    @Autowired
+    private ReservacionServicio reservacionServicio;
+
+
+    @GetMapping("/obtener/todos")
+    public List <Reservacion> obtenerTodos(){
+        return this.reservacionServicio.obtenerTodos();
+    }
+
+    @PostMapping("/crear/reservacion")
+        public Reservacion crearNuevaReservacion(@RequestBody Reservacion nuevaReservacion){
+            return this.reservacionServicio.crearNuevaReservacion(nuevaReservacion);
+        }
+
+}
