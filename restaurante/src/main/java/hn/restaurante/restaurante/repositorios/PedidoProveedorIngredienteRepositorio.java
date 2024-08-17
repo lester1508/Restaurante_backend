ea710c6393a545890225bea488cd39e715d01db4
lester1508 <140740945+lester1508@users.noreply.github.com>
2024-08-16 23:47:54 -0600

    Update and rename repositorioPedidoProveedorIngrediente.java to PedidoProveedorIngredienteRepositorio.java

--- restaurante/src/main/java/hn/restaurante/restaurante/repositorios/PedidoProveedorIngredienteRepositorio.java
+++ restaurante/src/main/java/hn/restaurante/restaurante/repositorios/PedidoProveedorIngredienteRepositorio.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import hn.restaurante.restaurante.modelos.PedidoProveedorIngrediente;
 import hn.restaurante.restaurante.modelos.PedidoProveedorIngredienteId;
 
-public interface repositorioPedidoProveedorIngrediente extends JpaRepository<PedidoProveedorIngrediente, PedidoProveedorIngredienteId> {
+public interface PedidoProveedorIngredienteRepositorio extends JpaRepository<PedidoProveedorIngrediente, PedidoProveedorIngredienteId> {
 }
