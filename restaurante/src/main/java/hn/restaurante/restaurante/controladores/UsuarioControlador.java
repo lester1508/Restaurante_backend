62e1ccbd6ae17e14ee0cf106a173abe89d5d1c7c
marianosorio <149555450+marianosorio@users.noreply.github.com>
2024-08-17 18:30:40 -0600

    Update UsuarioControlador.java

--- restaurante/src/main/java/hn/restaurante/restaurante/controladores/UsuarioControlador.java
+++ restaurante/src/main/java/hn/restaurante/restaurante/controladores/UsuarioControlador.java
@@ -54,6 +54,11 @@ public class UsuarioControlador {
 
         return this.usuarioServicio.buscarPorEmailContrasena(usuario);
     }
+
+    @GetMapping("obtener/{id}")
+    public Reservacion obtenerPorId(@PathVariable int id) {
+        return this.reservacionServicio.obtenerPorId(id);
+    }
 }
    
     
